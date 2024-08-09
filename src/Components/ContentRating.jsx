eddbbeafe264d70e8c5309f99257ea48ba14d61a
Magalark <namannagia@gmail.com>
2024-08-09 15:20:07 -0400

    adding final changes

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -6,15 +6,17 @@ class ContentRating extends Component {
   constructor() {
     super();
     this.state = {
-        likes: 0, dislikes: 0,
+        likes: 0, dislikes: 0, totalRatings: 0,
         handleLike:() => {
             this.setState((prevState) => ({
-                likes: prevState.likes + 1
+                likes: prevState.likes + 1,
+                totalRatings: prevState.totalRatings + 1
             }));
         },
         handleDislike:() =>{
             this.setState((prevState) => ({
-                dislikes: prevState.dislikes + 1
+                dislikes: prevState.dislikes + 1,
+                totalRatings: prevState.totalRatings + 1
             }));
         }
     };
@@ -27,12 +29,13 @@ class ContentRating extends Component {
         ---Add text here---
         </p>
         <div className='rating-buttons'>
-        <button className="like-button">
-            Like ({this.state.likes})
-          </button>
-          <button className="dislike-button">
-            Dislike ({this.state.dislikes})
-          </button>
+        <button className="like-button" onClick={this.state.handleLike}>
+                    Like ({this.state.likes})
+                </button>
+                <button className="dislike-button" onClick={this.state.handleDislike}>
+                    Dislike ({this.state.dislikes})
+        </button>
+        <p>Total Ratings: {this.state.totalRatings}</p>
         </div>
     </div>
      </>
